554d4d663ecd20b41138c7bdb8394cc30d147592
gatitochido8-sudo <gatitochido8@gmail.com>
2026-04-24 19:25:24 -0600

    Update calculadora.py

--- src/calculadora.py
+++ src/calculadora.py
@@ -3,10 +3,9 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b
-
+    
 def multiplicacion(a, b):
     return a * b
-
 def division(a, b):
     if b == 0:
         raise ValueError("No se puede dividir entre cero")
